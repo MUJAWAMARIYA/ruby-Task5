ed0f6e90f3fc0c6aed7a62a270191a85a3a1631f
mujawamariya <mumarg2015@gmail.com>
2019-07-26 00:27:20 -0700

    game task correction

--- sample1.rb
+++ sample1.rb
@@ -1,60 +1,51 @@
  class Player
- attr_accessor :hand
- def initialize(hand)
- @hand=(hand)
- end 
+ 
  def hand
-  return @hand
+
+  puts "enter the number you want"
+  hand=gets.to_i
+  while hand < 0 || hand > 3 do
+    puts "out of range try gain"
+    hand=gets.to_i
+  end
+
+      hand=gets.to_i
+       puts ("number choosen is #{hand}") 
+       hand
 end
 end
 
  class Enemy
- attr_accessor :hand
- def initialize(hand)
-    @hand=hand
- end
  def hand
-     return @hand
+ random_choice_from_enemy=rand(0..2)
+ puts "random value is #{random_choice_from_enemy}"
+ random_choice_from_enemy
 end
 end
 
 class Janken
-    attr_accessor :player_hand,:enemy_hand
-    def initialize(player_hand,enemy_hand)
-    @player_hand=player_hand
-    @enemy_hand=enemy_hand
-
-    end
+   
     def pon(player_hand,enemy_hand)
   display=(player_hand - enemy_hand +3)%3
   if(display==2)
-    return "win"
+    puts "you are a winner"
   elsif(display==1)
-    return "loose"
-  else
-return "draw"
+   puts  " you are looser"
+
+ else
+  puts "draw out do a gain"
+    player = Player.new
+	  enemy = Enemy.new
+	  janken = Janken.new
+	  janken.pon(player.hand, enemy.hand)
   end
 end
 end
+player = Player.new
+	  enemy = Enemy.new
+	  janken = Janken.new
+	  janken.pon(player.hand, enemy.hand)
 
-while(true) do
-
-puts "please select one among these following:"
-puts "0: Goo"
-puts "1: Choki"
-puts "2: Par"
-entered_number=gets.to_i
-if(entered_number<0 || entered_number>3)
-	puts "not specified range in game"
-else
-	player = Player.new(entered_number)
-	array_range=[0,1,2]
-	enemy_randomly=array_range[rand(array_range.count)];
-	enemy = Enemy.new(enemy_randomly)
-	janken = Janken.new(player,enemy)
-	puts " it is a #{janken.pon(player.hand,enemy.hand)}"
-end
-end
 
 
         
