4dd875686035ce83efd667cfa22167717edb7585
mujawamariya <mumarg2015@gmail.com>
2019-07-29 03:14:29 -0700

    another correction of ruby task5

--- sample1.rb
+++ sample1.rb
@@ -1,49 +1,43 @@
- class Player
- 
- def hand
-
-  puts "enter the number you want"
-  hand=gets.to_i
-  while hand < 0 || hand > 3 do
+class Player
+   def hand
+   puts "enter the number you want"
+   hand=gets.to_i
+    while hand < 0 || hand > 3 do
     puts "out of range try gain"
     hand=gets.to_i
+    end
+    hand=gets.to_i
+    puts ("number choosen is #{hand}") 
+    hand
+   end
+end
+class Enemy
+  def hand
+  random_choice_from_enemy=rand(0..2)
+  puts "random value is #{random_choice_from_enemy}"
+  random_choice_from_enemy
   end
-
-      hand=gets.to_i
-       puts ("number choosen is #{hand}") 
-       hand
 end
-end
-
- class Enemy
- def hand
- random_choice_from_enemy=rand(0..2)
- puts "random value is #{random_choice_from_enemy}"
- random_choice_from_enemy
-end
-end
-class Janken
-   
-    def pon(player_hand,enemy_hand)
+class Janken 
+  def pon(player_hand,enemy_hand)
   display=(player_hand - enemy_hand +3)%3
-  if(display==2)
+    if(display==2)
     puts "you are a winner"
-  elsif(display==1)
-   puts  " you are looser"
-
- else
-  puts "draw out do a gain"
+      elsif(display==1)
+      puts  " you are looser"
+    else
+    puts "draw out do a gain"
     player = Player.new
 	  enemy = Enemy.new
 	  janken = Janken.new
 	  janken.pon(player.hand, enemy.hand)
-  end
-end
+    end
+ end
 end
 player = Player.new
-	  enemy = Enemy.new
-	  janken = Janken.new
-	  janken.pon(player.hand, enemy.hand)
+enemy = Enemy.new
+janken = Janken.new
+janken.pon(player.hand, enemy.hand)
 
 
 
